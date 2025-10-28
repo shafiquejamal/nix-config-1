1a8b16a7a45a09129e009afee5b56e313895a9c9
Shafique Jamal <shafique.jamal@gmail.com>
2025-10-27 22:56:02 -0400

    Install Citrix

--- hosts/common/darwin-common.nix
+++ hosts/common/darwin-common.nix
@@ -1,13 +1,22 @@
-{ inputs, outputs, config, lib, hostname, system, username, pkgs, unstablePkgs, ... }:
-let
-  inherit (inputs) nixpkgs nixpkgs-unstable;
-in
 {
+  inputs,
+  outputs,
+  config,
+  lib,
+  hostname,
+  system,
+  username,
+  pkgs,
+  unstablePkgs,
+  ...
+}: let
+  inherit (inputs) nixpkgs nixpkgs-unstable;
+in {
   users.users.sj.home = "/Users/sj";
 
   nix = {
     settings = {
-      experimental-features = [ "nix-command" "flakes" ];
+      experimental-features = ["nix-command" "flakes"];
       warn-dirty = false;
     };
     channel.enable = false;
@@ -27,7 +36,7 @@ in
     # unstablePkgs.yt-dlp
     # unstablePkgs.get_iplayer
     unstablePkgs.colmena
-  
+
     ## stable CLI
     # pkgs.nix
     # firefox
@@ -76,6 +85,7 @@ in
       #"FelixKratz/formulae" #sketchybar
     ];
     casks = [
+      "citrix-workspace"
       "element"
       "firefox"
       "folx"
@@ -92,17 +102,17 @@ in
       "vlc"
       "whatsapp"
     ];
-      #    masApps = {
-#       "AutoMounter" = 1160435653;
-#      "Bitwarden" = 1352778147;
-#      "Disk Speed Test" = 425264550;
-#      "Resize Master" = 1025306797;
-#      "rCmd" = 1596283165;
-#      "Telegram" = 747648890;
-#      "The Unarchiver" = 425424353;
-#      "UTM" = 1538878817;
-#      "Wireguard" = 1451685025;
-## Also add WhatsApp. How about Google Meet?
+    #    masApps = {
+    #       "AutoMounter" = 1160435653;
+    #      "Bitwarden" = 1352778147;
+    #      "Disk Speed Test" = 425264550;
+    #      "Resize Master" = 1025306797;
+    #      "rCmd" = 1596283165;
+    #      "Telegram" = 747648890;
+    #      "The Unarchiver" = 425424353;
+    #      "UTM" = 1538878817;
+    #      "Wireguard" = 1451685025;
+    ## Also add WhatsApp. How about Google Meet?
     #    };
   };
 
@@ -131,93 +141,92 @@ in
   };
 
   system.defaults.CustomUserPreferences = {
-      "com.apple.finder" = {
-        ShowExternalHardDrivesOnDesktop = true;
-        ShowHardDrivesOnDesktop = false;
-        ShowMountedServersOnDesktop = false;
-        ShowRemovableMediaOnDesktop = true;
-        _FXSortFoldersFirst = true;
-        # When performing a search, search the current folder by default
-        FXDefaultSearchScope = "SCcf";
-        DisableAllAnimations = true;
-        NewWindowTarget = "PfDe";
-        NewWindowTargetPath = "file://$\{HOME\}/Desktop/";
-        AppleShowAllExtensions = true;
-        FXEnableExtensionChangeWarning = false;
-        ShowStatusBar = true;
-        ShowPathbar = true;
-        WarnOnEmptyTrash = false;
-      };
-      "com.apple.desktopservices" = {
-        # Avoid creating .DS_Store files on network or USB volumes
-        DSDontWriteNetworkStores = true;
-        DSDontWriteUSBStores = true;
-      };
-      "com.apple.HIToolbox" = {
-        # Completely disable Caps Lock functionality
-        AppleFnUsageType = 1; # Enable Fn key functionality
-        AppleKeyboardUIMode = 3;
-        # Disable Caps Lock toggle entirely
-        AppleSymbolicHotKeys = {
-          "60" = {
-            enabled = false; # Disable Caps Lock toggle hotkey
-          };
-        };
-        # Override modifier key behavior to prevent Caps Lock from functioning
-        AppleModifierKeyRemapping = {
-          "1452-630-0" = {
-            # Map Caps Lock (key code 57/0x39) to nothing (disable it)
-            HIDKeyboardModifierMappingSrc = 30064771129; # Caps Lock
-            HIDKeyboardModifierMappingDst = 30064771299; # No Action
-          };
+    "com.apple.finder" = {
+      ShowExternalHardDrivesOnDesktop = true;
+      ShowHardDrivesOnDesktop = false;
+      ShowMountedServersOnDesktop = false;
+      ShowRemovableMediaOnDesktop = true;
+      _FXSortFoldersFirst = true;
+      # When performing a search, search the current folder by default
+      FXDefaultSearchScope = "SCcf";
+      DisableAllAnimations = true;
+      NewWindowTarget = "PfDe";
+      NewWindowTargetPath = "file://$\{HOME\}/Desktop/";
+      AppleShowAllExtensions = true;
+      FXEnableExtensionChangeWarning = false;
+      ShowStatusBar = true;
+      ShowPathbar = true;
+      WarnOnEmptyTrash = false;
+    };
+    "com.apple.desktopservices" = {
+      # Avoid creating .DS_Store files on network or USB volumes
+      DSDontWriteNetworkStores = true;
+      DSDontWriteUSBStores = true;
+    };
+    "com.apple.HIToolbox" = {
+      # Completely disable Caps Lock functionality
+      AppleFnUsageType = 1; # Enable Fn key functionality
+      AppleKeyboardUIMode = 3;
+      # Disable Caps Lock toggle entirely
+      AppleSymbolicHotKeys = {
+        "60" = {
+          enabled = false; # Disable Caps Lock toggle hotkey
         };
       };
-      "com.apple.dock" = {
-        autohide = false;
-        launchanim = false;
-        static-only = false;
-        show-recents = false;
-        show-process-indicators = true;
-        orientation = "left";
-        tilesize = 36;
-        minimize-to-application = true;
-        mineffect = "scale";
-        enable-window-tool = false;
+      # Override modifier key behavior to prevent Caps Lock from functioning
+      AppleModifierKeyRemapping = {
+        "1452-630-0" = {
+          # Map Caps Lock (key code 57/0x39) to nothing (disable it)
+          HIDKeyboardModifierMappingSrc = 30064771129; # Caps Lock
+          HIDKeyboardModifierMappingDst = 30064771299; # No Action
+        };
       };
-      "com.apple.ActivityMonitor" = {
-        OpenMainWindow = true;
-        IconType = 5;
-        SortColumn = "CPUUsage";
-        SortDirection = 0;
-      };
-      "com.apple.Safari" = {
-        # Privacy: don’t send search queries to Apple
-        UniversalSearchEnabled = false;
-        SuppressSearchSuggestions = true;
-      };
-      "com.apple.AdLib" = {
-        allowApplePersonalizedAdvertising = false;
-      };
-      "com.apple.SoftwareUpdate" = {
-        AutomaticCheckEnabled = true;
-        # Check for software updates daily, not just once per week
-        ScheduleFrequency = 1;
-        # Download newly available updates in background
-        AutomaticDownload = 1;
-        # Install System data files & security updates
-        CriticalUpdateInstall = 1;
-      };
-      "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
-      # Prevent Photos from opening automatically when devices are plugged in
-      "com.apple.ImageCapture".disableHotPlug = true;
-      # Turn on app auto-update
-      "com.apple.commerce".AutoUpdate = true;
-      "com.googlecode.iterm2".PromptOnQuit = false;
-      "com.google.Chrome" = {
-        AppleEnableSwipeNavigateWithScrolls = true;
-        DisablePrintPreview = true;
-        PMPrintingExpandedStateForPrint2 = true;
-      };
+    };
+    "com.apple.dock" = {
+      autohide = false;
+      launchanim = false;
+      static-only = false;
+      show-recents = false;
+      show-process-indicators = true;
+      orientation = "left";
+      tilesize = 36;
+      minimize-to-application = true;
+      mineffect = "scale";
+      enable-window-tool = false;
+    };
+    "com.apple.ActivityMonitor" = {
+      OpenMainWindow = true;
+      IconType = 5;
+      SortColumn = "CPUUsage";
+      SortDirection = 0;
+    };
+    "com.apple.Safari" = {
+      # Privacy: don’t send search queries to Apple
+      UniversalSearchEnabled = false;
+      SuppressSearchSuggestions = true;
+    };
+    "com.apple.AdLib" = {
+      allowApplePersonalizedAdvertising = false;
+    };
+    "com.apple.SoftwareUpdate" = {
+      AutomaticCheckEnabled = true;
+      # Check for software updates daily, not just once per week
+      ScheduleFrequency = 1;
+      # Download newly available updates in background
+      AutomaticDownload = 1;
+      # Install System data files & security updates
+      CriticalUpdateInstall = 1;
+    };
+    "com.apple.TimeMachine".DoNotOfferNewDisksForBackup = true;
+    # Prevent Photos from opening automatically when devices are plugged in
+    "com.apple.ImageCapture".disableHotPlug = true;
+    # Turn on app auto-update
+    "com.apple.commerce".AutoUpdate = true;
+    "com.googlecode.iterm2".PromptOnQuit = false;
+    "com.google.Chrome" = {
+      AppleEnableSwipeNavigateWithScrolls = true;
+      DisablePrintPreview = true;
+      PMPrintingExpandedStateForPrint2 = true;
+    };
   };
-
 }
